6698621b82b0955eb3c0b1f9e769a80fbe68056a
Uttam3003 <133484684+Uttam3003@users.noreply.github.com>
2023-08-21 11:14:50 +0530

    Update RequireAssertRevert.sol

--- RequireAssertRevert.sol
+++ RequireAssertRevert.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract DivideByZero {
-    function divide(uint256 numerator, uint256 denominator) public pure returns (uint256) {
-        require(denominator != 0, "Division by zero is not allowed");
-        return numerator / denominator;
+contract SafeMathDivision {
+    function usingRequire(uint256 num) public pure returns (uint256) {
+        // Using require to check a condition
+        require(num > 0, "Number must be more than zero");
+        return num * 3;
     }
-    
-    function divideWithAssert(uint256 numerator, uint256 denominator) public pure returns (uint256) {
-        assert(denominator != 0);
-        return numerator / denominator;
+
+    function usingAssert(uint256 num) public pure returns (uint256) {
+        // Using assert to validate an internal invariant
+        assert(num != 0);
+        return num + 27;
     }
-    
-    function divideWithRevert(uint256 numerator, uint256 denominator) public pure returns (uint256) {
-        if (denominator == 0) {
-            revert("Division by zero is not allowed");
+
+    function usingRevert(uint256 num) public pure returns (uint256) {
+        // Using revert to revert state changes and provide a custom error message
+        if (num == 88) {
+            revert(" 88 is not allowed");
         }
-        return numerator / denominator;
+        return num - 1;
     }
 }
-
-
